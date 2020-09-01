0a06eb3cf68f69ee737d784224d19a0b77a57a01
Tim Finnigan <tim.finnigan1@gmail.com>
2020-09-01 15:32:36 -0400

    create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div id='header-container'>
+      <h3 id='main-header'>Five Takeaways</h3>
+      <nav id='main-nav'>nav to go here</nav>
+    </div>
+  );
+};
+
+export default Header;
