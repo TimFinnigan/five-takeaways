9bdd269422714936bd939b49c8249b10ef8f7b38
Tim Finnigan <tim.finnigan1@gmail.com>
2020-09-01 17:21:52 -0400

    add url to data

--- src/components/Books/BookList.js
+++ src/components/Books/BookList.js
@@ -1,13 +1,15 @@
 const LIST = [
   {
     title: 'Zero to One',
-    image: 'zero-to-one-peter-thiel.jpg',
     author: 'Peter Thiel',
+    image: 'zero-to-one-peter-thiel.jpg',
+    url: 'zero-to-one-peter-thiel',
   },
   {
     title: 'Mastery',
-    image: 'mastery-robert-greene.jpg',
     author: 'Robert Greene',
+    image: 'mastery-robert-greene.jpg',
+    url: 'mastery-robert-greene',
   },
 ];
 
