64602f34a38477365bc5af74256e2a1e21618793
Tim Finnigan <tim.finnigan1@gmail.com>
2020-09-01 16:29:16 -0400

    add book images

--- src/components/Books/BookList.js
+++ src/components/Books/BookList.js
@@ -1,12 +1,12 @@
 const LIST = [
   {
     title: 'Zero to One',
-    image: 'image-src',
+    image: 'zero-to-one-peter-thiel.jpg',
     author: 'Peter Thiel',
   },
   {
     title: 'Mastery',
-    image: 'image-src',
+    image: 'mastery-robert-greene.jpg',
     author: 'Robert Greene',
   },
 ];
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import LIST from './BookList';
+import './Books.css';
 
 const Books = () => {
   const items = LIST.map((book) => (
     <li key={book.title}>
+      <img
+        className='book'
+        src={process.env.PUBLIC_URL + '/images/books/' + book.image}
+        alt={book.title}
+      />
       <span>{book.title}</span>
       <span>{book.author}</span>
     </li>
