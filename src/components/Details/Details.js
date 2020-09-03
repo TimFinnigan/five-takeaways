142a7d8d12f59c30c28db5f336d03d723dcb0877
Tim Finnigan <tim.finnigan1@gmail.com>
2020-09-02 22:56:38 -0400

    add template for details component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,14 +2,30 @@ import React from 'react';
 import Header from '../Header/Header';
 import takeaways from './takeaways.json';
 
+import './Details.css';
+
 const Details = (props) => {
   const BOOK = props.match.params.book;
-  console.log(takeaways[BOOK].description);
   let description = takeaways[BOOK].description;
+  let one = takeaways[BOOK].one;
+  let two = takeaways[BOOK].two;
+  let three = takeaways[BOOK].three;
+  let four = takeaways[BOOK].four;
+  let five = takeaways[BOOK].five;
+
   return (
     <div>
       <Header />
-      {description}
+      <div id='book-container'>
+        <div id='book-description'>{description}</div>
+        <div id='lesson-container'>
+          <div className='lesson'>{one}</div>
+          <div className='lesson'>{two}</div>
+          <div className='lesson'>{three}</div>
+          <div className='lesson'>{four}</div>
+          <div className='lesson'>{five}</div>
+        </div>
+      </div>
     </div>
   );
 };
