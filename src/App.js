3c25a0daee10816b73b98561d3713796ea8c7b22
Tim Finnigan <tim.finnigan1@gmail.com>
2020-09-02 22:35:45 -0400

    add about component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import Details from './components/Details/Details';
+import About from './components/About/About';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
         <Route path='/:book' component={Details} />
       </Switch>
     </Router>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <div id='header-container'>
       <h3 id='main-header'>Five Takeaways</h3>
-      <nav id='main-nav'>nav to go here</nav>
+      <nav id='main-nav'>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </nav>
     </div>
   );
 };
