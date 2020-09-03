4a7f908a5c919e204c599ba58d2caedcbf671586
Tim Finnigan <tim.finnigan1@gmail.com>
2020-09-03 09:06:26 -0400

    add details to details component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -9,10 +9,15 @@ const Details = (props) => {
   let title = takeaways[BOOK].title;
   let description = takeaways[BOOK].description;
   let one = takeaways[BOOK].one;
+  let oneText = takeaways[BOOK].oneText;
   let two = takeaways[BOOK].two;
+  let twoText = takeaways[BOOK].twoText;
   let three = takeaways[BOOK].three;
+  let threeText = takeaways[BOOK].threeText;
   let four = takeaways[BOOK].four;
+  let fourText = takeaways[BOOK].fourText;
   let five = takeaways[BOOK].five;
+  let fiveText = takeaways[BOOK].fiveText;
 
   return (
     <div>
@@ -20,13 +25,43 @@ const Details = (props) => {
       <div className='details'>
         <div className='details__title'>{title}</div>
         <div className='details__description'>{description}</div>
-        <ol className='details__list'>
-          <li className='lesson'>{one}</li>
-          <li className='lesson'>{two}</li>
-          <li className='lesson'>{three}</li>
-          <li className='lesson'>{four}</li>
-          <li className='lesson'>{five}</li>
-        </ol>
+        <div className='details__list'>
+          <div className='details__lesson'>
+            <div className='details__lesson-header'>
+              {'1) '}
+              {one}
+            </div>
+            <div className='details__lesson-description'>{oneText}</div>
+          </div>
+          <div className='details__lesson'>
+            <div className='details__lesson-header'>
+              {'2) '}
+              {two}
+            </div>
+            <div className='details__lesson-description'>{twoText}</div>
+          </div>
+          <div className='details__lesson'>
+            <div className='details__lesson-header'>
+              {'3) '}
+              {three}
+            </div>
+            <div className='details__lesson-description'>{threeText}</div>
+          </div>
+          <div className='details__lesson'>
+            <div className='details__lesson-header'>
+              {'4) '}
+              {four}
+            </div>
+            <div className='details__lesson-description'>{fourText}</div>
+          </div>
+          <div className='details__lesson'>
+            <div className='details__lesson-header'>
+              {'5) '}
+              {five}
+            </div>
+            <div className='details__lesson-description'>{fiveText}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
