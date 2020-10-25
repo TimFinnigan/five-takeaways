24575dd00dac7751e5a1a657a0014c07183de642
Tim Finnigan <tim.finnigan1@gmail.com>
2020-10-25 08:18:34 -0400

    add ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Home from './components/Home/Home';
 import Details from './components/Details/Details';
 import About from './components/About/About';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/about' component={About} />
-        <Route path='/:book' component={Details} />
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <ScrollToTop />
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route path='/:book' component={Details} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
