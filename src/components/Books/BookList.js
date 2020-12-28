a4e4832dcd8d25e10425a668342cc83823254be3
TimFinnigan <tim.finnigan1@gmail.com>
2020-12-28 11:55:31 -0800

    add no rules rules book

--- src/components/Books/BookList.js
+++ src/components/Books/BookList.js
@@ -1,44 +1,49 @@
 const LIST = [
-  {
-    title: 'Atomic Habits',
-    author: 'James Clear',
-    url: 'atomic-habits-james-clear',
-  },
-  {
-    title: 'Range',
-    author: 'David Epstein',
-    url: 'range-david-epstein',
-  },
-  {
-    title: 'Ai Superpowers',
-    author: 'Kai-Fu Lee',
-    url: 'ai-superpowers-kai-fu-lee',
-  },
-  {
-    title: 'How to Win Friends and Influence People',
-    author: 'Dale Carnegie',
-    url: 'how-to-win-friends-and-influence-people-dale-carnegie',
-  },
-  {
-    title: 'Principles',
-    author: 'Ray Dalio',
-    url: 'principles-ray-dalio',
-  },
-  {
-    title: 'The Dip',
-    author: 'Seth Godin',
-    url: 'the-dip-seth-godin',
-  },
-  {
-    title: 'Zero to One',
-    author: 'Peter Thiel',
-    url: 'zero-to-one-peter-thiel',
-  },
-  {
-    title: 'Mastery',
-    author: 'Robert Greene',
-    url: 'mastery-robert-greene',
-  },
+	{
+		title: 'No Rules Rules',
+		author: 'Reed Hastings',
+		url: 'no-rules-rules-reed-hastings',
+	},
+	{
+		title: 'Atomic Habits',
+		author: 'James Clear',
+		url: 'atomic-habits-james-clear',
+	},
+	{
+		title: 'Range',
+		author: 'David Epstein',
+		url: 'range-david-epstein',
+	},
+	{
+		title: 'Ai Superpowers',
+		author: 'Kai-Fu Lee',
+		url: 'ai-superpowers-kai-fu-lee',
+	},
+	{
+		title: 'How to Win Friends and Influence People',
+		author: 'Dale Carnegie',
+		url: 'how-to-win-friends-and-influence-people-dale-carnegie',
+	},
+	{
+		title: 'Principles',
+		author: 'Ray Dalio',
+		url: 'principles-ray-dalio',
+	},
+	{
+		title: 'The Dip',
+		author: 'Seth Godin',
+		url: 'the-dip-seth-godin',
+	},
+	{
+		title: 'Zero to One',
+		author: 'Peter Thiel',
+		url: 'zero-to-one-peter-thiel',
+	},
+	{
+		title: 'Mastery',
+		author: 'Robert Greene',
+		url: 'mastery-robert-greene',
+	},
 ];
 
 export default LIST;
