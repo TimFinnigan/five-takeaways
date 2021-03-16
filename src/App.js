27d30f18769d538900d462f8f159c16a92e78027
TimFinnigan <tim.finnigan1@gmail.com>
2021-03-16 15:10:52 -0700

    create and add Subscribe component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Home from './components/Home/Home';
 import Details from './components/Details/Details';
 import About from './components/About/About';
+import Subscribe from './components/Subscribe/Subscribe';
 
 function App() {
-    return (
-        <Router>
-            <ScrollToTop />
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route path='/:book' component={Details} />
-            </Switch>
-        </Router>
-    );
+	return (
+		<Router>
+			<ScrollToTop />
+			<Switch>
+				<Route exact path='/' component={Home} />
+				{/* <Route exact path='/about' component={About} /> */}
+				<Route exact path='/subscribe' component={Subscribe} />
+				<Route path='/:book' component={Details} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,30 +3,37 @@ import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
-  return (
-    <div className='header'>
-      <NavLink exact className='header__link' to='/'>
-        <h3 className='header__main'>Five Takeaways</h3>
-      </NavLink>
-      <nav className='header__nav'>
-        <NavLink
-          exact
-          className='header__link'
-          activeStyle={{ color: 'grey' }}
-          to='/'
-        >
-          Home
-        </NavLink>
-        <NavLink
+	return (
+		<div className='header'>
+			<NavLink exact className='header__link' to='/'>
+				<h3 className='header__main'>Five Takeaways</h3>
+			</NavLink>
+			<nav className='header__nav'>
+				<NavLink
+					exact
+					className='header__link'
+					activeStyle={{ color: 'grey' }}
+					to='/'
+				>
+					Home
+				</NavLink>
+				{/* <NavLink
           className='header__link'
           activeStyle={{ color: 'grey' }}
           to='/about'
         >
           About
-        </NavLink>
-      </nav>
-    </div>
-  );
+        </NavLink> */}
+				<NavLink
+					className='header__link'
+					activeStyle={{ color: 'grey' }}
+					to='/subscribe'
+				>
+					Subscribe
+				</NavLink>
+			</nav>
+		</div>
+	);
 };
 
 export default Header;
